7f59741bafa1ef77072c59dd4555e486c24a4cd7
epaepke <epaepke@gmail.com>
2016-01-22 15:50:08 -0800

    Adds asteroid images to board

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,62 @@
 // start slingin' some d3 here.
+
+
+var astereoidData = [
+  {x: 5, y: 10},
+  {x: 50, y: 60},
+  {x: 150, y: 100},
+  {x: 200, y: 150}
+];
+
+// var astereoidData = {0: ;
+
+
+
+var selection = d3.select(".board").selectAll("div").data(astereoidData)
+
+var fnKey = function (d) {
+  console.log('d', d);
+
+};
+console.log(selection)
+
+
+// console.log(selection)
+
+// ENTER
+selection
+  .enter()
+  .append("svg")
+  .attr("width", 100)
+  .attr("height", 100)  
+  .append("image")
+  .attr("width", 100)
+  .attr("height", 100)
+  .attr("xlink:href", "asteroid.png")
+
+
+// EXIT
+selection
+  .exit()
+  .remove();
+
+
+var timer;
+var updatePositions = function(time) {
+  timer = setInterval( function(){
+    console.log('BANG!')
+
+    selection
+
+
+
+
+
+  },time)
+}
+
+var stopTimer = function() {
+  clearInterval(timer);
+}
+
+updatePositions(1000);
