ff0ef892da477215586a71233d45c1a7ee74e3d0
epaepke <epaepke@gmail.com>
2016-01-22 20:43:07 -0800

    Makes progress towards collision detection

--- client/watchout.js
+++ client/watchout.js
@@ -7,16 +7,19 @@
 // planet count,
 // planet size?
 
+var mouseStartPosition = [300, 100];
 // initiate asteroids data here with random creator!
 
 var astereoidData = [
-  {x: 5, y: 10},
-  {x: 50, y: 60},
-  {x: 150, y: 100},
-  {x: 200, y: 150}
+  {x: 600, y: 400}
+  // {x: 600, y: 400},
+  // {x: 600, y: 400},
+  // {x: 600, y: 400},
+  // {x: 600, y: 400},
+  // {x: 600, y: 400}
 ];
 
-var mousePositionData = [];
+var mousePositionData = mouseStartPosition;
 
 var selection;
 var width = 100;
@@ -64,8 +67,8 @@ initializeAsteroids();
 var addDraggableElement = function() {
   console.log('adds draggable elemt')
   d3.select(".board").append("image")
-    .attr("x", 150)
-    .attr("y", 100)
+    .attr("x", mouseStartPosition[0])
+    .attr("y", mouseStartPosition[1])
     .attr("height", "50px")
     .attr("width", "50px")
     .attr("xlink:href", "asteroid.png")
@@ -74,10 +77,9 @@ addDraggableElement();
 
 // declare mover function
 var mover = function() {
-  console.log('mover fires')
   var xPos = d3.event.x - parseInt(d3.select('image').attr("width")) / 2;
   var yPos = d3.event.y - parseInt(d3.select('image').attr("height")) / 2;
-  console.log('mover report', xPos, yPos)
+  recordNewMousePosition(xPos, yPos);
   d3.select('image')
     .attr("x", xPos)
     .attr("y", yPos);
@@ -86,26 +88,52 @@ var mover = function() {
 var drag = d3.behavior.drag()
   .on("drag", function(){
     mover();
-    recordNewMousePosition()
   });
 // call drag on image!
 d3.select(".board").select("image").call(drag);
 
+var recordNewMousePosition = function(x, y) {
+  // x value
+  mousePositionData[0] = x;
+  // y value
+  mousePositionData[1] = y;
+}
 
 var update = function () {
   // rebuild object with new datapoints
+ // var oldXAsteroidData = _.pluck(astereoidData, 'x');
+
+  // var oldAsteroidData = _.clone(astereoidData);
+  // console.log('old data', oldAsteroidData)
+
   for (var ast in astereoidData) {
     // console.log(astereoidData[ast].)
-    astereoidData[ast].x = Math.floor( (Math.random() - 0.5 ) * 1000) + 300;
-    astereoidData[ast].y = Math.floor( (Math.random() - 0.5 ) * 1000) + 300;
+    astereoidData[ast].x = Math.floor( (Math.random() - 0.5 ) * 1100) + 600;
+    astereoidData[ast].y = Math.floor( (Math.random() - 0.5 ) * 700) + 400;
     // console.log(astereoidData);
   }
+    // console.log('old data after ', oldAsteroidData);
+    // console.log('new data after ', astereoidData);
 
   selection
     .transition()
-    .duration(1000)
+    .duration(10000)
+    .tween("cx", function (d, t, i) {
+      var ast = d3.select(this);
+      var xStart = ast.attr("cx");
+      var yStart = ast.attr("cy");
+      var xEnd = d.x;
+      var yEnd = d.y;
+
+      return function (t) {
+        // console.log(mousePositionData);
+        
+      }
+    })
+
+    // })
     .attr("cx", function(d) {return d.x})
-    .attr("cy", function(d) {return d.y})
+    .attr("cy", function(d) {return d.y});
 };
 
 // init timer
@@ -121,4 +149,29 @@ var stopTimer = function() {
   clearInterval(timer);
 };
 
-updatePositions(1000);
+updatePositions(10000);
+
+
+
+
+
+
+
+
+
+
+// $(window).on('resize', function(){
+//   var cssWidthRule = $(window).width();
+//   $('body').css('width', cssWidthRule);
+// });
+
+
+
+
+
+
+
+
+
+
+
