c2d90af2bc2c8d1f61fe65e531136e2c7c43b6d5
Brian Fogg <fogg4444@gmail.com>
2016-01-23 16:52:05 -0800

    implemented dynamic height and changed colors

--- currency_visualizer/js/main.js
+++ currency_visualizer/js/main.js
@@ -1,20 +1,8 @@
 $(window).on('load', function(){
 
 
-  var width, height;
-
-  var resizeWindow = function() {
-    width = $(window).width();
-    height = $(window).height();
-    $('body').css('height', height);
-    $('body').css('width', width);
-    renderChart();
-  }
-
-  resizeWindow(); // run the first time
-  $(window).on('resize', resizeWindow);
-
   var url = "http://api.fixer.io/latest?base=USD"
+  var height, width;
 
   var ratesArray = [];
 
@@ -37,6 +25,61 @@ $(window).on('load', function(){
 
   getNewRates();
 
+  var renderChart = function() {
+    console.log('render chart')
+
+    var selected = d3.select('.currencyGraph')
+      .attr('background', 'blue')
+      .attr('height', '100%')
+      .selectAll('rect')
+      .data(ratesArray)
+      .attr('width', function(d, i){
+        console.log(d, i);
+        return d.rate * rateScalingFactor
+      })
+      .attr('height', function(d) {
+        return height / (ratesArray.length + 1) * 4 / 5;
+      })
+      .attr('y', function(d, i){
+        return (i) * height / (ratesArray.length);
+      })
+      .attr('x', 0)
+
+
+    selected
+      .enter()
+      .append('rect')
+      .transition()
+      .duration(1000)
+      .delay(function(d,i) {
+        return i * 100;
+      })
+      .attr('width', function(d, i){
+        console.log(d, i);
+        return Math.min(500, d.rate * rateScalingFactor)
+      })
+      .attr('height', function(d) {
+        return height / (ratesArray.length + 1) * 4 / 5;
+      })
+      .attr('y', function(d, i){
+        return (i) * height / (ratesArray.length + 1);
+      })
+      .attr('x', 0)
+  }
+
+
+  var resizeWindow = function() {
+    width = $(window).width();
+    height = $(window).height();
+    $('body').css('height', height);
+    $('body').css('width', width);
+    renderChart();
+  }
+
+  resizeWindow(); // run the first time
+  $(window).on('resize', resizeWindow);
+
+
   // takes data from api and forms it to our array requirements
   var processRates = function(input) {  
     var base = input.base;
@@ -51,28 +94,6 @@ $(window).on('load', function(){
     }
     console.log('process finish, render chart')
     renderChart();
-  }
-
-
-  var renderChart = function() {
-    console.log('render chart')
-
-    d3.select('.currencyGraph')
-      .attr('background', 'blue')
-      // .attr('height', '100%')
-      .selectAll('rect')
-      .data(ratesArray)
-      .enter()
-      .append('rect')
-      .attr('width', function(d, i){
-        console.log(d, i);
-        return d.rate * rateScalingFactor
-      })
-      .attr('height', rateHeight)
-      .attr('y', function(d, i){
-        return i * (rateHeight + verticalDistance)
-      })
-      .attr('x', 0)
   }
 
 
