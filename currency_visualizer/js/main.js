a630b5fe0766b1030960ff222ee5887ab2a1d68c
Brian Fogg <fogg4444@gmail.com>
2016-01-23 16:20:29 -0800

    implemented dynamic resizing of page with colored currency bars

--- currency_visualizer/js/main.js
+++ currency_visualizer/js/main.js
@@ -1,29 +1,80 @@
 $(window).on('load', function(){
-  console.log('yo js');
+
+
+  var width, height;
+
+  var resizeWindow = function() {
+    width = $(window).width();
+    height = $(window).height();
+    $('body').css('height', height);
+    $('body').css('width', width);
+    renderChart();
+  }
+
+  resizeWindow(); // run the first time
+  $(window).on('resize', resizeWindow);
 
   var url = "http://api.fixer.io/latest?base=USD"
 
-  var rates;
+  var ratesArray = [];
+
+  var rateScalingFactor = 20;
+  var rateHeight = 15;
+  var verticalDistance = 5;
 
   var getNewRates = function() {
     $.ajax(url, {
       success: function(a, b, c) {
-        console.log('success!');
-        rates = a.rates;
+        // console.log('success!');
         // console.log(rates)
-        renderNewRates(rates);
+        processRates(a);
       },
       error: function() {
-        console.log('failure');
+        // console.log('failure');
       }
     })
   }
 
   getNewRates();
 
-  var renderNewRates = function(rates) {
-    console.log(rates);
+  // takes data from api and forms it to our array requirements
+  var processRates = function(input) {  
+    var base = input.base;
+    var date = input.date;
+    var rates = input.rates;
+    for( var rate in rates ) {
+      var countryCode = rate;
+      var exRate = rates[rate];
+      var newObj = {country: countryCode,
+                    rate: exRate};
+      ratesArray.push(newObj);
+    }
+    console.log('process finish, render chart')
+    renderChart();
   }
+
+
+  var renderChart = function() {
+    console.log('render chart')
+
+    d3.select('.currencyGraph')
+      .attr('background', 'blue')
+      // .attr('height', '100%')
+      .selectAll('rect')
+      .data(ratesArray)
+      .enter()
+      .append('rect')
+      .attr('width', function(d, i){
+        console.log(d, i);
+        return d.rate * rateScalingFactor
+      })
+      .attr('height', rateHeight)
+      .attr('y', function(d, i){
+        return i * (rateHeight + verticalDistance)
+      })
+      .attr('x', 0)
+  }
+
 
 
 })
